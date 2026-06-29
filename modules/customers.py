353d5a27abe97dc36da94ac0c06060f091a71383
timokabz-cmd <timokabz@gmail.com>
2026-06-29 04:36:52 +0300

    Add customer management module with Streamlit

--- modules/customers.py
+++ modules/customers.py
@@ -1,1 +1,9 @@
+# modules/customers.py
+import streamlit as st
+from database.connection import get_db_connection
+
+def render():
+    st.write("#### 👥 Customer Management")
+    st.info("Customer module is now connected!")
+    # Your customer form code goes here
 
